eb81f8fe21f3b332a11bfcae4863082b0d5b6abb
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-06-20 22:55:44 +0300

    Created match component. Removed number in scores if match is not started yet

---
+++ app/components/ggf-tournament/match/component.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  score: function() {
+    let match = this.get('match');
+
+    const MATCH_STATUS_PLANNED = 'planned';
+
+    if (match.get('status') === MATCH_STATUS_PLANNED) {
+      match.set('homeScore', '-');
+      match.set('awayScore', '-');
+    }
+
+    return [match.get('homeScore'), match.get('awayScore')].join(' : ');
+  }.property()
+});
