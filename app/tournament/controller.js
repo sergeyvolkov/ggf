438c0c90c3230e60e5fb9d4388e2bdec678f83c4
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-06-23 20:00:27 +0300

    Got results for all matches: win, draw or lose

---
+++ app/tournament/controller.js
@@ -0,0 +1,49 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  tournament: Ember.A(),
+  matches:    Ember.A(),
+
+  matchesForTeam: function() {
+    // @todo need to merge with tablescore
+    let matches = this.get('matches'),
+        teams = this.get('tournament.teams'),
+        matchesForTeam = {};
+
+    teams.forEach(function(team) {
+      matchesForTeam[team] = [];
+    });
+
+    matches.forEach(function(match) {
+      let homeTeam = match.get('homeTeam'),
+          awayTeam = match.get('awayTeam'),
+          homeResult = '',
+          awayResult = '',
+          matchId = match.get('id');
+
+      if (match.get('status') !== 'finished') {
+        homeResult = awayResult = 'N'; // not finished yet
+      } else if (homeTeam > awayTeam) {
+        homeResult = 'W';
+        awayResult = 'L';
+      } else if (homeTeam < awayTeam) {
+        homeResult = 'L';
+        awayResult = 'W';
+      } else {
+        homeResult = awayResult = 'D';
+      }
+
+      matchesForTeam[homeTeam].push({
+        matchId:  matchId,
+        result:   homeResult
+      });
+
+      matchesForTeam[awayTeam].push({
+        matchId:  matchId,
+        result:   awayResult
+      });
+    });
+
+    console.warn(matchesForTeam, matches.get('length'));
+  }.property()
+});
