7b819847da82d6681dc783b86cd319c001d63747
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-06-24 01:16:09 +0300

    Started to create new start page

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('tournaments', {path: '/'}, function() {
+  this.resource('tournaments', function() {
     this.resource('tournament', {path: '/tournament/:tournamentId'});
   });
 });
