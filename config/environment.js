54ff0554c5aeb9d4b6267ca9de0ebae57695156e
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-06-24 01:17:56 +0300

    Fixed csp for google fonts

--- config/environment.js
+++ config/environment.js
@@ -16,6 +16,10 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    contentSecurityPolicy: {
+      'font-src': "'self' data: fonts.gstatic.com",
+      'style-src': "'self' 'unsafe-inline' fonts.googleapis.com"
     }
   };
 
