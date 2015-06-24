b6eb7266c7c49dcaeb48cf1e1a8953b6007380f1
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-06-24 22:47:06 +0300

    Replaced application to index route. Moved code from old template to new

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('index', {path: '/'});
+
   this.resource('tournaments', function() {
     this.resource('tournament', {path: '/tournament/:tournamentId'});
   });
