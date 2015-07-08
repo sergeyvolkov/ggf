863a89e409036b762736c2140f1f224b2615562b
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-07-08 09:46:21 +0300

    After-merge fix

--- app/helpers/dasherize-string.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export function dasherizeString(params) {
-  let str = params[0];
-  return str.dasherize();
-}
-
-export default Ember.HTMLBars.makeBoundHelper(dasherizeString);
