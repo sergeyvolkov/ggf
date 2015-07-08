581f962daf0876d5aefd5e7fe2d061885d623d43
Pavel Machekhin <pavel.machekhin@gmail.com>
2015-07-08 13:25:27 +0300

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -4,17 +4,15 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 cd $DIR/../
 
-npm install --dev
+# Install npm dependencies
+echo "NPM Install [backend]"
+npm install
 
 cd $DIR/../resources/frontend
 
 # Install npm dependencies
-echo "NPM Install"
+echo "NPM Install [frontend]"
 npm install
-
-# Install bower dependencies
-echo "Bower Install"
-./node_modules/.bin/bower install
 
 # Build Ember Application
 echo "Build Ember (env: ${EMBER_ENV})"
