536ec03378e389d16d1d71694172aa9a478d998a
Pavel Machekhin <pavel.machekhin@gmail.com>
2015-07-10 09:20:27 +0300

    Add entity member_tokens

---
+++ database/migrations/2015_07_09_153813_member_access_tokens.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MemberAccessTokens extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member_tokens', function (Blueprint $table) {
+            $table->integer('memberId');
+            $table->string('accessToken');
+            $table->string('sessionId');
+            $table->timestamps();
+        });
+
+        Schema::table('member_tokens', function(Blueprint $table) {
+            $table->foreign('memberId')
+                ->references('id')
+                ->on('members')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('member_tokens', function(Blueprint $table) {
+            $table->dropForeign('member_tokens_memberId_foreign');
+        });
+
+        Schema::drop('member_tokens');
+    }
+}
