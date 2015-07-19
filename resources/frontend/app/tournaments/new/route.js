6afcf5ecdb7222a31c0ee62cd2503ef598d1cb30
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-07-20 01:05:15 +0300

    Saved tournament to local store

--- resources/frontend/app/tournaments/new/route.js
+++ resources/frontend/app/tournaments/new/route.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController(controller) {
+    const newTournament = {
+      name: '',
+      description: '',
+      type: '',
+      membersType: ''
+    };
+
+    controller.set('tournament', newTournament);
+  },
+
+  actions: {
+    saveTournament(tournament) {
+      this.store.createRecord('tournament', tournament);
+    }
+  }
 });
