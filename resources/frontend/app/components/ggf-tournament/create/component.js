6d77ff01f31e6f7d98cba72540b1107126e9374c
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-07-22 03:27:13 +0300

    Created ggf-tournament.create component. Got all new properties

---
+++ resources/frontend/app/components/ggf-tournament/create/component.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  name:         '',
+  description:  '',
+  type:         'league', // tournament type
+  membersType:  'single', // single or double
+
+  actions: {
+    saveTournament() {
+      const params = this.getProperties('name', 'description', 'type', 'membersType');
+    }
+  }
+});
