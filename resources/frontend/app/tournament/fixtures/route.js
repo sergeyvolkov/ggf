a3adb008e1d2d3b296443049cd797ef2d62415eb
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-08-02 15:44:38 +0300

    Moved to settings page on complete setup button click

--- resources/frontend/app/tournament/fixtures/route.js
+++ resources/frontend/app/tournament/fixtures/route.js
@@ -6,5 +6,11 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
   setupController(controller) {
     const tournamentStatus = this.controllerFor('tournament').get('status');
     controller.set('tournamentStatus', tournamentStatus);
+  },
+
+  actions: {
+    goToSettings() {
+      this.transitionTo('tournament.settings');
+    }
   }
 });
--- resources/frontend/app/tournament/matches/route.js
+++ resources/frontend/app/tournament/matches/route.js
@@ -6,5 +6,12 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
   setupController(controller) {
     const tournamentStatus = this.controllerFor('tournament').get('status');
     controller.set('tournamentStatus', tournamentStatus);
+  },
+
+  actions: {
+    goToSettings() {
+      this.transitionTo('tournament.settings');
+    }
   }
+
 });
