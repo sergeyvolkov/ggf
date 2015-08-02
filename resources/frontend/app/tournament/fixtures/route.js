773eff0b0719b16f8774f97f6bbd577a2414b97b
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-08-02 16:59:34 +0300

    Got matches list in fixture and matches routes. Used matches-list component

--- resources/frontend/app/tournament/fixtures/route.js
+++ resources/frontend/app/tournament/fixtures/route.js
@@ -3,7 +3,14 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
-  setupController(controller) {
+  model() {
+    const tournamentId = this.modelFor('tournament').get('id');
+    return this.store.find('match', {tournamentId, status: 'not_started'});
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+
     const tournamentStatus = this.controllerFor('tournament').get('status');
     controller.set('tournamentStatus', tournamentStatus);
   },
