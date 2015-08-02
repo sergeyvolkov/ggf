d8a114682d902998ff0b5b82447cfd53ce75995e
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-08-02 17:26:35 +0300

    Refactored score in matches-list component

--- resources/frontend/app/components/ggf-tournament/matches-list/component.js
+++ resources/frontend/app/components/ggf-tournament/matches-list/component.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // set score variable for each match
+  matchesList: function() {
+    const matches = this.get('matches');
+
+    return matches.map( (match) => {
+      const score = this.getMatchScore(match);
+
+      match.set('score', score);
+
+      return match;
+    } );
+  }.property('matches'),
+
+  getMatchScore(match) {
+    const homeScore = (match.get('status') !== 'not_started') ? match.get('homeScore') : '-';
+    const awayScore = (match.get('status') !== 'not_started') ? match.get('awayScore') : '-';
+
+    return `${homeScore} : ${awayScore}`;
+  }
 });
