0d359b86b1c0f3f6e97c03d341eee00d995eb223
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-08-03 22:04:11 +0300

    Moved all resources under pods directory

--- resources/frontend/config/environment.js
+++ resources/frontend/config/environment.js
@@ -4,6 +4,7 @@ module.exports = function (environment) {
 
   var ENV = {
     modulePrefix: 'ggf',
+    podModulePrefix: 'ggf/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'hash',
