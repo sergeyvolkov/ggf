0b86032ce4c089a5c3deaeb27ea9deb2976002e7
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-08-03 22:22:20 +0300

    Removed centered-column-header view. Removed templates and views directories

--- resources/frontend/app/views/centered-column-header.js
+++
@@ -1,7 +0,0 @@
-import DefaultHeaderView from 'ember-cli-materialize/views/default-column-header';
-import layout from '../templates/centered-column-header';
-
-export default DefaultHeaderView.extend({
-  classNameBindings: [':centered', ':min'],
-  layout
-});
