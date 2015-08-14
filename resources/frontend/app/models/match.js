43f4d01584bc595c2154750b57b6b808a482286f
Sergey Volkov <volkov.sergey@nixsolutions.com>
2015-08-14 15:04:24 +0300

    Added round attribute for match model

--- resources/frontend/app/models/match.js
+++ resources/frontend/app/models/match.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  round:        DS.attr('number'),
   homeTeam:     DS.attr('string'),
   awayTeam:     DS.attr('string'),
   homeScore:    DS.attr('number'),
