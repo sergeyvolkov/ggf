4586bd5ad68eb33e3993813829e2166e965a8f95
Pavel Machekhin <pavel.machekhin@gmail.com>
2015-09-24 12:23:20 +0300

    Remove unwise methods from md-reveal-button component

--- resources/frontend/app/pods/components/ggf-reveal-menu/component.js
+++ resources/frontend/app/pods/components/ggf-reveal-menu/component.js
@@ -1,39 +1,15 @@
 import Ember from 'ember';
-import MaterializeButton from 'ember-cli-materialize/components/md-btn';
+import MaterializeButtonDropdown from 'ember-cli-materialize/components/md-btn-dropdown';
 import layout from './template';
 
 const { run: { scheduleOnce }, computed } = Ember;
 
-export default MaterializeButton.extend({
+export default MaterializeButtonDropdown.extend({
   layout,
-  tagName: 'a',
   text: 'Button',
   classNames: ['dropdown-button', 'btn-floating', 'reveal-menu'],
   icon: 'mdi-navigation-more-vert',
-  iconPosition: 'right',
   attributeBindings: [
     'inDuration', 'outDuration', 'hover', 'gutter', 'belowOrigin'
-  ],
-
-  didInsertElement() {
-    this._super(...arguments);
-    scheduleOnce('afterRender', this, this._setupDropdown);
-  },
-
-  _setupDropdown() {
-    // needed until the Materialize.dropdown plugin is replaced
-    this.$().attr('data-activates', this.get('_dropdownContentId'));
-
-    this.$().dropdown({
-      hover: !!this.getWithDefault('hover', false),
-      constrain_width: false,
-      inDuration: this.getWithDefault('inDuration', this.get('_mdSettings.dropdownInDuration')),
-      outDuration: this.getWithDefault('outDuration', this.get('_mdSettings.dropdownOutDuration')),
-      gutter: this.getWithDefault('gutter', 0),
-      belowOrigin: !!this.getWithDefault('belowOrigin', false)
-    });
-  },
-  _dropdownContentId: computed(function() {
-    return `${this.get('elementId')}-dropdown-content`;
-  })
+  ]
 });
