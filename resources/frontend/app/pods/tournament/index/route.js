966ee9d8b4ce516e8979f4b67f2ea4d52f1369d0
Pavel Machekhin <pavel.machekhin@gmail.com>
2015-10-09 20:54:40 +0300

    Cleanup

--- resources/frontend/app/pods/tournament/index/route.js
+++ resources/frontend/app/pods/tournament/index/route.js
@@ -4,15 +4,6 @@ export default Ember.Route.extend({
 
   beforeModel: function() {
     return this.transitionTo('tournament.standings');
-  },
-  
-  setupController (controller, model, transition) {
-    let tournamentId = transition.params.tournament.tournamentId;
-    let tournament = this.store.peekAll('tournament').findBy('id', tournamentId);
-
-    controller.set('tournament', tournament);
-    controller.set('matches', model);
-
-    this._super(controller, model, transition);
   }
+
 });
