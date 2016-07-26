73de415f908a31f9b12fff0c68413c212b6be295
Pavel Machekhin <pavel.machekhin@gmail.com>
2016-07-27 00:31:19 +0300

    Fix multiple teams seeding with NationalTeams2Seeder

--- database/seeds/NationalTeams2Seeder.php
+++ database/seeds/NationalTeams2Seeder.php
@@ -17,41 +17,55 @@ class NationalTeams2Seeder extends Seeder
         ]);
         $league->save();
 
-        DB::table('teams')->insert([
-            'leagueId' => $league->id,
-            'name' => 'Austria',
-            'logoPath' => 'teams-logo/austria.png',
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now()
-        ]);
-        DB::table('teams')->insert([
-            'leagueId' => $league->id,
-            'name' => 'Denmark',
-            'logoPath' => 'teams-logo/denmark.png',
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now()
-        ]);
-        DB::table('teams')->insert([
-            'leagueId' => $league->id,
-            'name' => 'Greece',
-            'logoPath' => 'teams-logo/greece.png',
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now()
-        ]);
-        DB::table('teams')->insert([
-            'leagueId' => $league->id,
-            'name' => 'Portugal',
-            'logoPath' => 'teams-logo/portugal.png',
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now()
-        ]);
-        DB::table('teams')->insert([
-            'leagueId' => $league->id,
-            'name' => 'Switzerland',
-            'logoPath' => 'teams-logo/switzerland.png',
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now()
-        ]);
+        if (1 > \App\Models\Team::where(['name' => 'Austria'])->count()) {
+            DB::table('teams')->insert([
+                'leagueId' => $league->id,
+                'name' => 'Austria',
+                'logoPath' => 'teams-logo/austria.png',
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now()
+            ]);
+        }
+
+        if (1 > \App\Models\Team::where(['name' => 'Denmark'])->count()) {
+            DB::table('teams')->insert([
+                'leagueId' => $league->id,
+                'name' => 'Denmark',
+                'logoPath' => 'teams-logo/denmark.png',
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now()
+            ]);
+        }
+
+        if (1 > \App\Models\Team::where(['name' => 'Greece'])->count()) {
+            DB::table('teams')->insert([
+                'leagueId' => $league->id,
+                'name' => 'Greece',
+                'logoPath' => 'teams-logo/greece.png',
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now()
+            ]);
+        }
+
+        if (1 > \App\Models\Team::where(['name' => 'Portugal'])->count()) {
+            DB::table('teams')->insert([
+                'leagueId' => $league->id,
+                'name' => 'Portugal',
+                'logoPath' => 'teams-logo/portugal.png',
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now()
+            ]);
+        }
+
+        if (1 > \App\Models\Team::where(['name' => 'Switzerland'])->count()) {
+            DB::table('teams')->insert([
+                'leagueId' => $league->id,
+                'name' => 'Switzerland',
+                'logoPath' => 'teams-logo/switzerland.png',
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now()
+            ]);
+        }
 
     }
 }
